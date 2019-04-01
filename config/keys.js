7b70522b58c6b4da61a4eb69cbf27e0fe9e087af
Roderick Trice <rocky_trice@yahoo.com>
2019-04-01 11:34:39 -0400

    connect MongoDB and added some middleware

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Rocky:Lionel1024@sneakerhut-ek32d.mongodb.net/test?retryWrites=true"
+
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const http = require("http");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// db config
+const db = require("../config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db).then(() => console.log("🗄  MongoDB Connected"))
+    .catch(err => console.log(err));
+
+// middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Sever Setup
 const PORT = process.env.PORT || 3002;
 const server = http.createServer(app);
 server.listen(PORT, () => console.log(`🌎  Server listening on port ${PORT}`));
